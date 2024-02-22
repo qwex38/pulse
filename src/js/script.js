672ecb7d9895054ffaa1524c3ba60f640478099e
qwex38 <gl4dyshev@mail.ru>
2024-02-22 18:53:00 +0800

    error js

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: <button type="button" class="slick-prev"><img src="../icons/back.svg"></button>,
+    });
+});
