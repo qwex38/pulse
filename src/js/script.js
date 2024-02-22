ca0cbe2571eca780e3129f3b1bc0593233706db3
qwex38 <gl4dyshev@mail.ru>
2024-02-22 21:46:42 +0800

    add carousel

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,33 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
         speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: <button type="button" class="slick-prev"><img src="../icons/back.svg"></button>,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/back.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                infinite: true,
+                dots: true,
+                arrows: false,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+
     });
 });
